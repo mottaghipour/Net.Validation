09ddd614d501f891365c661907b28eb857d71f08
AmirAbbas <mottaghipour@hotmail.com>
2022-02-19 18:46:48 +0330

    Create tests for "StringNullReferenceException" ("Valtion.Core.Tests")

---
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringNullReferenceException/StringNullReferenceException.Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions.StringNullReferenceException
+{
+    public class StringNullReferenceException
+    {
+
+    }
+}
