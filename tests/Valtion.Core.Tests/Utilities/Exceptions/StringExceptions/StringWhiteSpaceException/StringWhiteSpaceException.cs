52aab8656252e0e61e813269931b84c165964bab
AmirAbbas <mottaghipour@hotmail.com>
2022-02-19 21:56:00 +0330

    Create tests for "StringWhiteSpaceException" ("Valtion.Core.Tests")

---
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringWhiteSpaceException/StringWhiteSpaceException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
+using Xunit;
+
+namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
+{
+    public class StringWhiteSpaceException_Tests
+    {
+
+    }
+}
