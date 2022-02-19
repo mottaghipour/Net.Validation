fedbb7b28e06ed26722d134db66b37a1249d7fea
AmirAbbas <mottaghipour@hotmail.com>
2022-02-19 19:05:59 +0330

    Clean "StringNullReferenceException_Tests" ("Valtion.Core.Tests")

--- tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringNullReferenceException/StringNullReferenceException.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringNullReferenceException/StringNullReferenceException.Tests.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
-
 using Valtion.Core.Utilities.Exceptions.StringExceptions;
 
 namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
