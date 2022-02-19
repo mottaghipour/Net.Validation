7e82346e6cd98d7e103bfcbc9d3cdcd37ef4a854
AmirAbbas <mottaghipour@hotmail.com>
2022-02-19 19:03:08 +0330

    Test method "StringNullReferenceException_Tests" ("Valtion.Core.Tests")

--- tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringNullReferenceException/StringNullReferenceException.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringNullReferenceException/StringNullReferenceException.Tests.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
-namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions.StringNullReferenceException
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
+
+namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
 {
-    public class StringNullReferenceException
+    public class StringNullReferenceException_Tests
     {
+        [Fact]
+        public void Throw_StringNullReferenceException_Without_Parameters()
+        {
+            Action Throw = () => throw new StringNullReferenceException();
 
+            Assert.Throws<StringNullReferenceException>(Throw);
+        }
     }
 }
