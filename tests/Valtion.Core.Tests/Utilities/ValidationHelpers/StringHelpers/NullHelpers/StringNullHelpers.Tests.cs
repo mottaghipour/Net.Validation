93aa69ffbb07a4461626fcda6e274e70929951ab
AmirAbbas <mottaghipour@hotmail.com>
2022-02-23 11:48:15 +0330

    StringNullHelpers_Tests "String_ShouldNotBeNull_Valid" ("Valtion.Core.Tests")

--- tests/Valtion.Core.Tests/Utilities/ValidationHelpers/StringHelpers/NullHelpers/StringNullHelpers.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/ValidationHelpers/StringHelpers/NullHelpers/StringNullHelpers.Tests.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
+using Valtion.Core.Utilities.ValidationHelpers.StringHelpers;
+using Xunit;
 
 namespace Valtion.Core.Tests.Utilities.ValidationHelpers.StringHelpers
 {
     public class StringNullHelpers_Tests
     {
+        [Fact]
+        public void String_ShouldNotBeNull_Valid()
+        {
+            string value = "Value";
 
+            string result = value.ShouldNotBeNull();
+
+            Assert.Equal(value, result);
+        }
     }
 }
