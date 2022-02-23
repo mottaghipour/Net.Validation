4b6ef688cc6a0d2be9f6cc39fef5e4afe066d881
AmirAbbas <mottaghipour@hotmail.com>
2022-02-23 11:41:21 +0330

    StringNullHelpers "ShouldBeNull(string value)" ("Valtion.Core")

--- src/Valtion.Core/Utilities/ValidationHelpers/StringHelpers/NullHelpers/StringNullHelpers.cs
+++ src/Valtion.Core/Utilities/ValidationHelpers/StringHelpers/NullHelpers/StringNullHelpers.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
 
 namespace Valtion.Core.Utilities.ValidationHelpers.StringHelpers
 {
     public static class StringNullHelpers
     {
+        public static string ShouldBeNull(this string value)
+        {
+            if (value == null)
+                StringNullReferenceException.Throw();
 
+            return value;
+        }
     }
 }
