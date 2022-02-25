dc77066bf36a03c5cd328669882e4d962118d2e5
AmirAbbas <mottaghipour@hotmail.com>
2022-02-25 10:11:36 +0330

    StringFullReferenceException_Tests "Throw_StringFullReferenceException_Without_Parameters" ("Valtion.Core")

--- tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringFullReferenceException/StringFullReferenceException.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringFullReferenceException/StringFullReferenceException.Tests.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
+using Xunit;
 
 namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
 {
     public class StringFullReferenceException_Tests
     {
+        [Fact]
+        public void Throw_StringFullReferenceException_Without_Parameters()
+        {
+            //Action Throw = () => throw new StringFullReferenceException();
 
+            //Assert.Throws<StringFullReferenceException>(Throw);
+        }
     }
 }
