7beb18c8b8d9528132b83c5ac5821f30fcf6e7a4
AmirAbbas <mottaghipour@hotmail.com>
2022-02-26 11:23:11 +0330

    StringIsNotWhiteSpaceException_Tests "Throw_StringIsNotWhiteSpaceException_Without_Parameters" ("Valtion.Core.Tests")

--- tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringIsNotWhiteSpaceException/StringIsNotWhiteSpaceException.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringIsNotWhiteSpaceException/StringIsNotWhiteSpaceException.Tests.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
+using Xunit;
 
 namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
 {
     public class StringIsNotWhiteSpaceException_Tests
     {
+        [Fact]
+        public void Throw_StringIsNotWhiteSpaceException_Without_Parameters()
+        {
+            //Action Throw = () => throw new StringIsNotWhiteSpaceException();
 
+            //Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
+        }
     }
 }
