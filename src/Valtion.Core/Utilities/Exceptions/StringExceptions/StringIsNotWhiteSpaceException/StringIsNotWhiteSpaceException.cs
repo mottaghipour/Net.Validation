15023b27ceed74cb51bf3e1bc00374dff044f1ad
AmirAbbas <mottaghipour@hotmail.com>
2022-02-26 11:30:35 +0330

    Create class and ctor"StringIsNotWhiteSpaceException" ("Valtion.Core")

---
+++ src/Valtion.Core/Utilities/Exceptions/StringExceptions/StringIsNotWhiteSpaceException/StringIsNotWhiteSpaceException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valtion.Core.Utilities.Exceptions.StringExceptions
+{
+    public class StringIsNotWhiteSpaceException
+    {
+        /// <summary>
+        /// Contractor <see cref="StringIsNotWhiteSpaceException"/> without parameters
+        /// </summary>
+        public StringIsNotWhiteSpaceException() : base() { }
+    }
+}
