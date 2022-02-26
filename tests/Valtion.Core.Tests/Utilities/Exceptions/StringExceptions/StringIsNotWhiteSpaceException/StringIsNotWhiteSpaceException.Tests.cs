aa0da78389d88991dc28ef8c5068f53adab0a4c9
AmirAbbas <mottaghipour@hotmail.com>
2022-02-26 11:36:42 +0330

    StringIsNotWhiteSpaceException_Tests passed "Valtion.Core.Tests"

--- tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringIsNotWhiteSpaceException/StringIsNotWhiteSpaceException.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringIsNotWhiteSpaceException/StringIsNotWhiteSpaceException.Tests.cs
@@ -21,63 +21,63 @@ namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
         [Fact]
         public void Throw_StringIsNotWhiteSpaceException_With_Message()
         {
-            //string message = "String is not white space exception occurred;";
+            string message = "String is not white space exception occurred;";
 
-            //Action Throw = () => throw new StringIsNotWhiteSpaceException(message);
+            Action Throw = () => throw new StringIsNotWhiteSpaceException(message);
 
-            //Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
+            Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
         }
 
         [Fact]
         public void Throw_StringIsNotWhiteSpaceException_With_MessageAndInnerException()
         {
-            //try
-            //{
-            //    throw new StringIsNotWhiteSpaceException();
-            //}
-            //catch (StringIsNotWhiteSpaceException innerException)
-            //{
-            //    string message = "String is not white space exception occurred;";
+            try
+            {
+                throw new StringIsNotWhiteSpaceException();
+            }
+            catch (StringIsNotWhiteSpaceException innerException)
+            {
+                string message = "String is not white space exception occurred;";
 
-            //    Action Throw = () => throw new StringIsNotWhiteSpaceException(message, innerException);
+                Action Throw = () => throw new StringIsNotWhiteSpaceException(message, innerException);
 
-            //    Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
-            //}
+                Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
+            }
         }
 
         [Fact]
         public void ThrowMethod_StringIsNotWhiteSpaceException_Without_Parameters()
         {
-            //Action Throw = () => StringIsNotWhiteSpaceException.Throw();
+            Action Throw = () => StringIsNotWhiteSpaceException.Throw();
 
-            //Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
+            Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
         }
 
         [Fact]
         public void ThrowMethod_StringIsNotWhiteSpaceException_With_Message()
         {
-            //string message = "String is not white space exception occurred;";
+            string message = "String is not white space exception occurred;";
 
-            //Action Throw = () => StringIsNotWhiteSpaceException.Throw(message);
+            Action Throw = () => StringIsNotWhiteSpaceException.Throw(message);
 
-            //Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
+            Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
         }
 
         [Fact]
         public void ThrowMethod_StringIsNotWhiteSpaceException_With_MessageAndInnerException()
         {
-            //try
-            //{
-            //    StringIsNotWhiteSpaceException.Throw();
-            //}
-            //catch (StringIsNotWhiteSpaceException innerException)
-            //{
-            //    string message = "String is not white space exception occurred;";
+            try
+            {
+                StringIsNotWhiteSpaceException.Throw();
+            }
+            catch (StringIsNotWhiteSpaceException innerException)
+            {
+                string message = "String is not white space exception occurred;";
 
-            //    Action Throw = () => StringIsNotWhiteSpaceException.Throw(message, innerException);
+                Action Throw = () => StringIsNotWhiteSpaceException.Throw(message, innerException);
 
-            //    Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
-            //}
+                Assert.Throws<StringIsNotWhiteSpaceException>(Throw);
+            }
         }
     }
 }
