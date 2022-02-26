43056b32fe2e887f668e49ab7e2ecf181cb992d1
AmirAbbas <mottaghipour@hotmail.com>
2022-02-26 12:38:06 +0330

    StringValidators "Required(value, message)" ("Valtion")

---
+++ src/Valtion/Validators/StringValidators/Required/StringRequiredValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
+using Valtion.Core.Utilities.ValidationHelpers.StringHelpers;
+
+namespace Valtion.Validators.StringValidators
+{
+    public static partial class StringValidators
+    {
+        /// <summary>
+        /// String required validator
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="message"></param>
+        /// <returns><see cref="string"/> <paramref name="value"/></returns>
+        /// <exception cref="StringNullReferenceException"></exception>
+        /// <exception cref="StringWhiteSpaceException"></exception>
+        public static string Required(this string value, string message = "This field is required;")
+        {
+            return value.ShouldNotBeNull(message).ShouldNotBeWhiteSpace(message);
+        }
+    }
+}
