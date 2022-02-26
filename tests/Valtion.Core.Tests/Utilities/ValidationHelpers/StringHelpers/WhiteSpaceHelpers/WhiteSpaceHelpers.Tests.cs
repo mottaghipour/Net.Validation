e0f8216138e111076bb030387cb391516daac644
AmirAbbas <mottaghipour@hotmail.com>
2022-02-26 11:44:40 +0330

    WhiteSpaceHelpers_Tests "String_ShouldBeWhiteSpace_Valid" ("Valtion.Core.Tests")

--- tests/Valtion.Core.Tests/Utilities/ValidationHelpers/StringHelpers/WhiteSpaceHelpers/WhiteSpaceHelpers.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/ValidationHelpers/StringHelpers/WhiteSpaceHelpers/WhiteSpaceHelpers.Tests.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.ValidationHelpers.StringHelpers;
+using Xunit;
 
 namespace Valtion.Core.Tests.Utilities.ValidationHelpers.StringHelpers
 {
     public class WhiteSpaceHelpers_Tests
     {
+        [Fact]
+        public void String_ShouldBeWhiteSpace_Valid()
+        {
+            string? value = " ";
 
+            string result = value.ShouldBeWhiteSpace();
+
+            Assert.Equal(value, result);
+        }
     }
 }
