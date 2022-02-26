633515474b54af1117f2b1621bcc2564bf2cda90
AmirAbbas <mottaghipour@hotmail.com>
2022-02-26 11:40:42 +0330

    WhiteSpaceHelpers "ShouldBeWhiteSpace(value, message)" ("Valtion.Core")

--- src/Valtion.Core/Utilities/ValidationHelpers/StringHelpers/WhiteSpaceHelpers/WhiteSpaceHelpers.cs
+++ src/Valtion.Core/Utilities/ValidationHelpers/StringHelpers/WhiteSpaceHelpers/WhiteSpaceHelpers.cs
@@ -10,6 +10,21 @@ namespace Valtion.Core.Utilities.ValidationHelpers.StringHelpers
     public static class WhiteSpaceHelpers
     {
         /// <summary>
+        /// Helper => String should be white space
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="message"></param>
+        /// <returns><see cref="string"/> <paramref name="value"/></returns>
+        /// <exception cref="StringIsNotWhiteSpaceException"></exception>
+        public static string ShouldBeWhiteSpace(this string value, string message = "String white space exception occurred;")
+        {
+            if (value != " ")
+                StringIsNotWhiteSpaceException.Throw(message);
+
+            return value;
+        }
+
+        /// <summary>
         /// Helper => String should not be white space
         /// </summary>
         /// <param name="value"></param>
