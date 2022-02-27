c9ce25c3fa1954c939a5608719f3c963ac051a87
AmirAbbas <mottaghipour@hotmail.com>
2022-02-27 11:00:39 +0330

    StringEmptyHelpers_Tests "String_ShouldBeEmpty_InValid" ("Valtion.Core.Tests")

--- tests/Valtion.Core.Tests/Utilities/ValidationHelpers/StringHelpers/EmptyHelpers/StringEmptyHelpers.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/ValidationHelpers/StringHelpers/EmptyHelpers/StringEmptyHelpers.Tests.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
 using Xunit;
 
 namespace Valtion.Core.Tests.Utilities.ValidationHelpers.StringHelpers
 {
-    public class StringEmptyHelpers
+    public class StringEmptyHelpers_Tests
     {
         [Fact]
         public void String_ShouldBeEmpty_Valid()
@@ -17,6 +18,16 @@ namespace Valtion.Core.Tests.Utilities.ValidationHelpers.StringHelpers
             //string result = value.ShouldBeEmpty();
 
             //Assert.Equal(value, result);
+        }
+
+        [Fact]
+        public void String_ShouldBeEmpty_InValid()
+        {
+            //string? value = "value";
+
+            //Action Throw = () => value.ShouldBeEmpty();
+
+            //Assert.Throws<StringIsNotEmptyException>(Throw);
         }
     }
 }
