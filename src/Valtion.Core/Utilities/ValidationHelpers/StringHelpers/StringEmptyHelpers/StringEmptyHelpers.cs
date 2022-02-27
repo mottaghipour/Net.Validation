6e7a9739db33abaca7ccde77afcd629494536be1
AmirAbbas <mottaghipour@hotmail.com>
2022-02-27 11:12:24 +0330

    StringEmptyHelpers "ShouldBeEmpty(value, message)" ("Valtion.Core")

--- src/Valtion.Core/Utilities/ValidationHelpers/StringHelpers/StringEmptyHelpers/StringEmptyHelpers.cs
+++ src/Valtion.Core/Utilities/ValidationHelpers/StringHelpers/StringEmptyHelpers/StringEmptyHelpers.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
 
 namespace Valtion.Core.Utilities.ValidationHelpers.StringHelpers
 {
     public static class StringEmptyHelpers
     {
+        /// <summary>
+        /// Helper => String should be empty
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns><see cref="string"/> <paramref name="value"/></returns>
+        /// <exception cref="StringIsNotEmptyException"></exception>
+        public static string ShouldBeEmpty(this string value, string message = "String is not empty exception occurred;")
+        {
+            if (value != "")
+                StringIsNotEmptyException.Throw(message);
 
+            return value;
+        }
     }
 }
