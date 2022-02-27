6547ab0e22924a82817332b83c8d884fcfc16e73
AmirAbbas <mottaghipour@hotmail.com>
2022-02-27 10:46:28 +0330

    StringIsNotEmptyException "ctor()" ("Valtion.Core")

---
+++ src/Valtion.Core/Utilities/Exceptions/StringExceptions/StringIsNotEmptyException/StringIsNotEmptyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valtion.Core.Utilities.Exceptions.StringExceptions
+{
+    public class StringIsNotEmptyException : Exception
+    {
+        /// <summary>
+        /// Contractor <see cref="StringIsNotEmptyException"/> without parameters
+        /// </summary>
+        public StringIsNotEmptyException() : base() { }
+    }
+}
