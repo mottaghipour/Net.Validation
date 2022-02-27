96b9435fae16e59889ab9531b79e57bd21f89f9a
AmirAbbas <mottaghipour@hotmail.com>
2022-02-27 10:24:45 +0330

    StringIsNotEmptyException_Tests "Throw_StringIsNotEmptyException_Without_Parameters" ("Valtion.Core.Tests")

---
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringIsNotEmptyException/StringIsNotEmptyException.Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
+{
+    public class StringIsNotEmptyException_Tests
+    {
+        [Fact]
+        public void Throw_StringIsNotEmptyException_Without_Parameters()
+        {
+            //Action Throw = () => throw new StringIsNotEmptyException();
+
+            //Assert.Throws<StringIsNotEmptyException>(Throw);
+        }
+    }
+}
