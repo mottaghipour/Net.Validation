d9f7bef4f7c15bef7fc6525dfe2f8b8614603b6e
AmirAbbas <mottaghipour@hotmail.com>
2022-02-27 10:58:55 +0330

    StringEmptyHelpers "String_ShouldBeEmpty_Valid" ("Valtion.Core.Tests")

---
+++ tests/Valtion.Core.Tests/Utilities/ValidationHelpers/StringHelpers/EmptyHelpers/StringEmptyHelpers.Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Valtion.Core.Tests.Utilities.ValidationHelpers.StringHelpers
+{
+    public class StringEmptyHelpers
+    {
+        [Fact]
+        public void String_ShouldBeEmpty_Valid()
+        {
+            //string? value = "";
+
+            //string result = value.ShouldBeEmpty();
+
+            //Assert.Equal(value, result);
+        }
+    }
+}
