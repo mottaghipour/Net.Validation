a9ca056282371059be55eeec9d849afa22d0cbd9
AmirAbbas <mottaghipour@hotmail.com>
2022-02-27 10:51:39 +0330

    StringIsNotEmptyException_Tests passed ("Valtion.Core.Tests")

--- tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringIsNotEmptyException/StringIsNotEmptyException.Tests.cs
+++ tests/Valtion.Core.Tests/Utilities/Exceptions/StringExceptions/StringIsNotEmptyException/StringIsNotEmptyException.Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
 using Xunit;
 
 namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
@@ -12,71 +13,71 @@ namespace Valtion.Core.Tests.Utilities.Exceptions.StringExceptions
         [Fact]
         public void Throw_StringIsNotEmptyException_Without_Parameters()
         {
-            //Action Throw = () => throw new StringIsNotEmptyException();
+            Action Throw = () => throw new StringIsNotEmptyException();
 
-            //Assert.Throws<StringIsNotEmptyException>(Throw);
+            Assert.Throws<StringIsNotEmptyException>(Throw);
         }
 
         [Fact]
         public void Throw_StringIsNotEmptyException_With_Message()
         {
-            //string message = "String is not empty exception occurred;";
+            string message = "String is not empty exception occurred;";
 
-            //Action Throw = () => throw new StringIsNotEmptyException(message);
+            Action Throw = () => throw new StringIsNotEmptyException(message);
 
-            //Assert.Throws<StringIsNotEmptyException>(Throw);
+            Assert.Throws<StringIsNotEmptyException>(Throw);
         }
 
         [Fact]
         public void Throw_StringIsNotEmptyException_With_MessageAndInnerException()
         {
-            //try
-            //{
-            //    throw new StringIsNotEmptyException();
-            //}
-            //catch (StringIsNotEmptyException innerException)
-            //{
-            //    string message = "String is not empty exception occurred;";
+            try
+            {
+                throw new StringIsNotEmptyException();
+            }
+            catch (StringIsNotEmptyException innerException)
+            {
+                string message = "String is not empty exception occurred;";
 
-            //    Action Throw = () => throw new StringIsNotEmptyException(message, innerException);
+                Action Throw = () => throw new StringIsNotEmptyException(message, innerException);
 
-            //    Assert.Throws<StringIsNotEmptyException>(Throw);
-            //}
+                Assert.Throws<StringIsNotEmptyException>(Throw);
+            }
         }
 
         [Fact]
         public void ThrowMethod_StringIsNotEmptyException_Without_Parameters()
         {
-            //Action Throw = () => StringIsNotEmptyException.Throw();
+            Action Throw = () => StringIsNotEmptyException.Throw();
 
-            //Assert.Throws<StringIsNotEmptyException>(Throw);
+            Assert.Throws<StringIsNotEmptyException>(Throw);
         }
 
         [Fact]
         public void ThrowMethod_StringIsNotEmptyException_With_Message()
         {
-            //string message = "String is not empty exception occurred;";
+            string message = "String is not empty exception occurred;";
 
-            //Action Throw = () => StringIsNotEmptyException.Throw(message);
+            Action Throw = () => StringIsNotEmptyException.Throw(message);
 
-            //Assert.Throws<StringIsNotEmptyException>(Throw);
+            Assert.Throws<StringIsNotEmptyException>(Throw);
         }
 
         [Fact]
         public void ThrowMethod_StringIsNotEmptyException_With_MessageAndInnerException()
         {
-            //try
-            //{
-            //    StringIsNotEmptyException.Throw();
-            //}
-            //catch (StringIsNotEmptyException innerException)
-            //{
-            //    string message = "String is not empty exception occurred;";
+            try
+            {
+                StringIsNotEmptyException.Throw();
+            }
+            catch (StringIsNotEmptyException innerException)
+            {
+                string message = "String is not empty exception occurred;";
 
-            //    Action Throw = () => StringIsNotEmptyException.Throw(message, innerException);
+                Action Throw = () => StringIsNotEmptyException.Throw(message, innerException);
 
-            //    Assert.Throws<StringIsNotEmptyException>(Throw);
-            //}
+                Assert.Throws<StringIsNotEmptyException>(Throw);
+            }
         }
     }
 }
