77f61aa6aadbdb3d47a0ae271cd76f1e3097499c
AmirAbbas <mottaghipour@hotmail.com>
2022-02-28 16:23:45 +0330

    Create test "StringRequiredValidator_Tests" ("Valtion.Core")

---
+++ tests/Valtion.Tests/Validators/StringValidators/Required/StringRequiredValidator.Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valtion.Tests.Validators.StringValidators
+{
+    public class StringRequiredValidator_Tests
+    {
+    }
+}
