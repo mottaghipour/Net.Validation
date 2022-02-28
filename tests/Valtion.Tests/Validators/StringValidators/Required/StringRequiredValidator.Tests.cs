f16231a655514df4e52c7d93f5f3b070114fcb86
AmirAbbas <mottaghipour@hotmail.com>
2022-02-28 16:35:03 +0330

    StringRequiredValidator_Tests "String_Required_Valid" ("Valtion.Tests")

--- tests/Valtion.Tests/Validators/StringValidators/Required/StringRequiredValidator.Tests.cs
+++ tests/Valtion.Tests/Validators/StringValidators/Required/StringRequiredValidator.Tests.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Validators.StringValidators;
+using Xunit;
 
 namespace Valtion.Tests.Validators.StringValidators
 {
     public class StringRequiredValidator_Tests
     {
+        [Fact]
+        public void String_Required_Valid()
+        {
+            string value = "String value";
+
+            string result = value.Required();
+
+            Assert.Equal(value, result);
+        }
     }
 }
