376cbc439cd8840afdf32b7e2d4045fe7691ed52
AmirAbbas <mottaghipour@hotmail.com>
2022-02-28 16:39:15 +0330

    StringRequiredValidator_Tests "String_Required_With_NullValue_ThrowException" ("Valtion.Tests")

--- tests/Valtion.Tests/Validators/StringValidators/Required/StringRequiredValidator.Tests.cs
+++ tests/Valtion.Tests/Validators/StringValidators/Required/StringRequiredValidator.Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Valtion.Core.Utilities.Exceptions.StringExceptions;
 using Valtion.Validators.StringValidators;
 using Xunit;
 
@@ -11,13 +12,23 @@ namespace Valtion.Tests.Validators.StringValidators
     public class StringRequiredValidator_Tests
     {
         [Fact]
-        public void String_Required_Valid()
+        public void String_Required_With_ValidValue()
         {
             string value = "String value";
 
             string result = value.Required();
 
             Assert.Equal(value, result);
+        }
+
+        [Fact]
+        public void String_Required_With_NullValue_ThrowException()
+        {
+            string? value = null;
+
+            Action action = () => value.Required();
+
+            Assert.Throws<StringNullReferenceException>(action);
         }
     }
 }
