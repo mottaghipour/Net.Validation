abb74e640a7c05dcdf512941f953545700e168f3
AmirAbbas <mottaghipour@hotmail.com>
2022-03-08 15:02:53 +0330

    Add IsNull

---
+++ src/Condition.Helpers/BaseHelpers/StringHelpers/NullHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nilgon.Net.Condition.Helpers
+{
+	public static partial class StringHelpers
+	{
+		/// <summary>
+        /// IsNull
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+		public static bool IsNull(this string value)
+        {
+			return value == null;
+        }
+	}
+}
+
