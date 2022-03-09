0f21c80f3ca1aab44afbc0937376c2156d3ebb90
AmirAbbas <mottaghipour@hotmail.com>
2022-03-09 19:44:32 +0330

    String is null method

---
+++ src/Condition.Helpers/ConditionHelpers/StringConditionHelpers/NullHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Nilgon.Net.Condition.Helpers
+{
+	public static partial class StringConditionHelpers
+	{
+		public static bool IsNull(this string value)
+        {
+			return value == null;
+        }
+	}
+}
+
