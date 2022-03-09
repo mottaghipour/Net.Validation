b2fed2ae5fcb84400c92ea7fc8db19ae31068112
AmirAbbas <mottaghipour@hotmail.com>
2022-03-09 19:36:10 +0330

    Test method “IsNull_WithFullValue_ReturnFalse”

---
+++ tests/Condition.Helpers.Tests/ConditionHelpers/StringConditionHelpers/NullHelpers.Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace Condition.Helpers.Tests.ConditionHelpers.StringConditionHelpers
+{
+	public class NullHelpers
+	{
+        [Fact]
+        public void IsNull_WithNullValue_ReturnTrue()
+        {
+            //string? value = null;
+
+            //bool result = value.IsNull();
+
+            //Assert.True(result);
+        }
+
+        [Fact]
+        public void IsNull_WithFullValue_ReturnFalse()
+        {
+            //string? value = null;
+
+            //bool result = value.IsNull();
+
+            //Assert.False(result);
+        }
+    }
+}
