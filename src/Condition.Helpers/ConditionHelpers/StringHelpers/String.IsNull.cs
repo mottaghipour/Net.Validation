b11f8f7d408ea18a836bc2bacf4b70dfe3260eda
AmirAbbas <mottaghipour@hotmail.com>
2022-03-11 15:49:45 +0330

    String is null method

---
+++ src/Condition.Helpers/ConditionHelpers/StringHelpers/String.IsNull.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nilgon.Net.Condition.Helpers
+{
+    public static partial class StringConditionHelpers
+    {
+        /// <summary>
+        /// Indicates whether the specified <see cref="string"/> is <see langword="null"/>.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns><see langword="true"/> if the <paramref name="value"/> parameter is <see langword="null"/>. otherwise, <see langword="false"/>.</returns>
+        public static bool IsNull(this string value)
+        {
+            string.IsNullOrEmpty(value);
+            return value == null;
+        }
+    }
+}
