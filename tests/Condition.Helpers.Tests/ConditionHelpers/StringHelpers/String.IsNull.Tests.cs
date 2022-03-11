46ca7c155da8e3794d113ffc505483f01bd74514
AmirAbbas <mottaghipour@hotmail.com>
2022-03-11 15:49:57 +0330

    String is null method tests

---
+++ tests/Condition.Helpers.Tests/ConditionHelpers/StringHelpers/String.IsNull.Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nilgon.Net.Condition.Helpers.StringHelpers.Tests
+{
+    public class StringIsNullTests
+    {
+        [Fact]
+        public void NullString_ReturnTrue()
+        {
+            string value = null;
+
+            bool result = value.IsNull();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void FullString_ReturnFalse()
+        {
+            string value = "String";
+
+            bool result = value.IsNull();
+
+            Assert.False(result);
+        }
+    }
+}
