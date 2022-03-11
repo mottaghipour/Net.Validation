028424b38e799345fb7414ccba7bfc151a20dc27
AmirAbbas <mottaghipour@hotmail.com>
2022-03-11 18:23:52 +0330

    String is empty method

---
+++ src/Condition.Helpers/ConditionHelpers/StringHelpers/String.IsEmpty.cs
@@ -0,0 +1,15 @@
+﻿namespace Nilgon.Net.Condition.Helpers
+{
+	public static partial class StringConditionHelpers
+	{
+        /// <summary>
+        /// Indicates whether the specified <see cref="string"/> is empty string("").
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns><see langword="true"/> if the <paramref name="value"/> parameter is empty string(""). otherwise, <see langword="false"/>.</returns>
+        public static bool IsEmpty(this string value)
+        {
+			return value == string.Empty;
+        }
+	}
+}
