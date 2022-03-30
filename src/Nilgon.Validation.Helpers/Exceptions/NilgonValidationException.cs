4a224bf57d0386d3fd7348f28f5b2a6756b5829d
AmirAbbas <mottaghipour@hotmail.com>
2022-03-30 10:52:44 +0430

    NilgonValidationException Create methods

--- src/Nilgon.Validation.Helpers/Exceptions/NilgonValidationException.cs
+++ src/Nilgon.Validation.Helpers/Exceptions/NilgonValidationException.cs
@@ -8,19 +8,34 @@
 
         private NilgonValidationException(string? message, Exception? innerException) : base(message, innerException) { }
 
+        public static NilgonValidationException Create() 
+        {
+            return new NilgonValidationException();
+        }
+
+        public static NilgonValidationException Create(string? message)
+        {
+            return new NilgonValidationException(message);
+        }
+
+        public static NilgonValidationException Create(string? message, Exception? innerException)
+        {
+            return new NilgonValidationException(message, innerException);
+        }
+
         public static string Throw()
         {
-            throw new NilgonValidationException();
+            throw Create();
         }
 
         public static string Throw(string? message)
         {
-            throw new NilgonValidationException(message);
+            throw Create(message);
         }
 
         public static string Throw(string? message, Exception? innerException)
         {
-            throw new NilgonValidationException(message, innerException);
+            throw Create(message, innerException);
         }
     }
 }
