f365e08dd2279604524504bd73195181f083adbe
AmirAbbas <mottaghipour@hotmail.com>
2022-04-14 09:33:55 +0430

    Create "ExceptionManager"

---
+++ src/Nilgon.Validation.Helpers/Utilities/ExceptionManager.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nilgon.Validation.Helpers.Utilities
+{
+    internal static class ExceptionManager
+    {
+        /// <summary>
+        ///    Throws the specified exception.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <exception cref="ValidationException"></exception>
+        internal static void ThrowValidationException(string? message)
+        {
+            var validationException = CreateValidationException(message);
+
+            throw validationException;
+        }
+
+        /// <summary>
+        ///   Creates the validation exception.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>
+        ///   The <see cref="ValidationException" />.
+        /// </returns>
+        private static ValidationException CreateValidationException(string? message)
+        {
+            return new ValidationException(message);
+        }
+    }
+}
