4ea486bc184c4940e0c8c0e0179ce7a0194841c0
AmirAbbas <mottaghipour@hotmail.com>
2022-05-14 07:07:25 +0430

    [TEST] Adding Tests “NullHelpers” => CommonValidationHelpers

---
+++ tests/Nilgon.Validation.Helpers.Tests/ValidationHelpers/CommonValidationHelpers/CommonValidationHelpers.NullHelpers/MustBeNullValidationHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nilgon.Validation.Helpers.Tests.ValidationHelpers.CommonValidationHelpers.NullHelpers;
+
+public sealed class MustBeNull_Validation_Helper_Tests
+{
+    [Fact]
+    public void MustBeNull_ReturnsValue_WhenObjectIsNull()
+    {
+        object objectToTest = null;
+
+        object result = objectToTest.MustBeNull();
+
+        Assert.Equal(objectToTest, result);
+    }
+
+    [Fact]
+    public void MustBeNull_ThrowException_WhenObjectIsNotNull()
+    {
+        object objectToTest = string.Empty;
+
+        Action action = () => objectToTest.MustBeNull();
+
+        Assert.Throws<ValidationException>(action);
+    }
+}
---
+++ tests/Nilgon.Validation.Helpers.Tests/ValidationHelpers/CommonValidationHelpers/CommonValidationHelpers.NullHelpers/MustNotBeNullValidationHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace Nilgon.Validation.Helpers.Tests.ValidationHelpers.CommonValidationHelpers.NullHelpers;
+
+public class MustNotBeNull_Validation_Helper_Tests
+{
+    [Fact]
+    public void MustNotBeNull_ReturnsValue_WhenObjectIsNotNull()
+    {
+        object objectToTest = string.Empty;
+
+        object result = objectToTest.MustNotBeNull();
+
+        Assert.Equal(objectToTest, result);
+    }
+
+    [Fact]
+    public void MustNotBeNull_ThrowException_WhenObjectIsNull()
+    {
+        object objectToTest = null;
+
+        Action action = () => objectToTest.MustNotBeNull();
+
+        Assert.Throws<ValidationException>(action);
+    }
+}
