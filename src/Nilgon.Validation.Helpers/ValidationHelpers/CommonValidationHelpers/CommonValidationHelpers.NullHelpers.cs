a89dd361d74d38aea85d584f73143f14af50beb0
AmirAbbas <mottaghipour@hotmail.com>
2022-05-14 06:58:49 +0430

    [FEATURE] Adding “NullHelpers” => CommonValidationHelpers

---
+++ src/Nilgon.Validation.Helpers/ValidationHelpers/CommonValidationHelpers/CommonValidationHelpers.NullHelpers.cs
@@ -0,0 +1,28 @@
+﻿using Nilgon.Condition.Helpers;
+
+namespace Nilgon.Validation.Helpers;
+
+public static partial class CommonValidationHelpers
+{
+    private const string MustBeNullMessage = "The value must be null.";
+    public static T MustBeNull<T>(this T value, string? message = MustBeNullMessage)
+    {
+        if (value.IsNotNull())
+        {
+            ThrowException(message);
+        }
+
+        return value;
+    }
+
+    private const string MustNotBeNullMessage = "The value must not be null.";
+    public static T MustNotBeNull<T>(this T value, string? message = MustNotBeNullMessage)
+    {
+        if (value.IsNull())
+        {
+            ThrowException(message);
+        }
+
+        return value;
+    }
+}
