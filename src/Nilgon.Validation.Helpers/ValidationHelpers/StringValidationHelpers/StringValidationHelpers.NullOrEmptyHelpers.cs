a375e503cc6c0d6d3d70c0782ecb5de0dba14b5c
AmirAbbas <mottaghipour@hotmail.com>
2022-05-15 06:50:21 +0430

    [FEATURE] Adding “NullOrEmptyHelpes” => SttringValidationHelpers

---
+++ src/Nilgon.Validation.Helpers/ValidationHelpers/StringValidationHelpers/StringValidationHelpers.NullOrEmptyHelpers.cs
@@ -0,0 +1,28 @@
+﻿using Nilgon.Condition.Helpers;
+
+namespace Nilgon.Validation.Helpers;
+
+public static partial class StringValidationHelpers
+{
+    private const string MustBeNullOrEmptyMessage = "test message";
+    public static string MustBeNullOrEmpty(this string value, string? message = MustBeNullOrEmptyMessage)
+    {
+        if (value.IsNotNullOrEmpty())
+        {
+            ThrowException(message);
+        }
+
+        return value;
+    }
+
+    private const string MustNotBeNullOrEmptyMessage = "test message";
+    public static string MustNotBeNullOrEmpty(this string value, string? message = MustNotBeNullOrEmptyMessage)
+    {
+        if (value.IsNullOrEmpty())
+        {
+            ThrowException(message);
+        }
+
+        return value;
+    }
+}
