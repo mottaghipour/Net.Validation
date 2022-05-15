f83349f5c099d8904f821fe2a61b44114c8e2d24
AmirAbbas <mottaghipour@hotmail.com>
2022-05-15 06:28:32 +0430

    [FEATURE] Adding “EmptyHelpers” => StringValidationHelpers

---
+++ src/Nilgon.Validation.Helpers/ValidationHelpers/StringValidationHelpers/StringValidationHelpers.EmptyHelpers.cs
@@ -0,0 +1,28 @@
+﻿using Nilgon.Condition.Helpers;
+
+namespace Nilgon.Validation.Helpers;
+
+public static partial class StringValidationHelpers
+{
+    private const string MustBeEmptyMessage = "test message";
+    public static string MustBeEmpty(this string value, string? message = MustBeEmptyMessage)
+    {
+        if (value.IsNotEmpty())
+        {
+            ThrowException(message);
+        }
+
+        return value;
+    }
+
+    private const string MustNotBeEmptyMessage = "test message";
+    public static string MustNotBeEmpty(this string value, string? message = MustNotBeEmptyMessage)
+    {
+        if (value.IsEmpty())
+        {
+            ThrowException(message);
+        }
+
+        return value;
+    }
+}
