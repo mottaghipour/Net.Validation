7db3999aef3faf84d8f44d51a5fd69341ca015e6
AmirAbbas <mottaghipour@hotmail.com>
2022-05-15 08:45:12 +0430

    [TEST] Tests passed "NullOrEmptyHelpers" => StringValidationHelpers

---
+++ tests/Nilgon.Validation.Helpers.Tests/ValidationHelpers/StringValidationHelpers/StringValidationHelpers.NullOrEmptyHelpers/MustBeNullOrEmptyValidationHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nilgon.Validation.Helpers.Tests.ValidationHelpers.StringValidationHelpers.NullOrEmptyHelpers;
+
+public sealed class MustBeNullOrEmpty_Validation_Helper_Tests
+{
+    [Fact]
+    public void MustBeNullOrEmpty_ReturnsValue_WhenStringIsNull()
+    {
+        string stringToTest = null;
+        
+        string result = stringToTest.MustBeNullOrEmpty();
+
+        Assert.Equal(stringToTest, result);
+    }
+
+    [Fact]
+    public void MustBeNullOrEmpty_ReturnsValue_WhenStringIsEmpty()
+    {
+        string stringToTest = string.Empty;
+
+        string result = stringToTest.MustBeNullOrEmpty();
+
+        Assert.Equal(stringToTest, result);
+    }
+
+    [Fact]
+    public void MustBeNullOrEmpty_ThrowsException_WhenStringIsWhiteSpace()
+    {
+        string stringToTest = " ";
+
+        Action action = () => stringToTest.MustBeNullOrEmpty();
+
+        Assert.Throws<ValidationException>(action);
+    }
+
+    [Fact]
+    public void MustBeNullOrEmpty_ThrowsException_WhenStringIsFull()
+    {
+        string stringToTest = "test";
+
+        Action action = () => stringToTest.MustBeNullOrEmpty();
+
+        Assert.Throws<ValidationException>(action);
+    }
+}
---
+++ tests/Nilgon.Validation.Helpers.Tests/ValidationHelpers/StringValidationHelpers/StringValidationHelpers.NullOrEmptyHelpers/MustNotBeNullOrEmptyValidationHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nilgon.Validation.Helpers.Tests.ValidationHelpers.StringValidationHelpers.NullOrEmptyHelpers;
+
+public sealed class MustNotBeNullOrEmpty_Validation_Helper_Tests
+{
+    [Fact]
+    public void MustNotBeNullOrEmpty_ReturnsValue_WhenStringIsWhiteSpace()
+    {
+        string stringToTest = " ";
+
+        string result = stringToTest.MustNotBeNullOrEmpty();
+
+        Assert.Equal(stringToTest, result);
+    }
+
+    [Fact]
+    public void MustNotBeNullOrEmpty_ReturnsValue_WhenStringIsFull()
+    {
+        string stringToTest = "test";
+
+        string result = stringToTest.MustNotBeNullOrEmpty();
+
+        Assert.Equal(stringToTest, result);
+    }
+
+    [Fact]
+    public void MustNotBeNullOrEmpty_ThrowsException_WhenStringIsNull()
+    {
+        string stringToTest = null;
+
+        Action action = () => stringToTest.MustNotBeNullOrEmpty();
+
+        Assert.Throws<ValidationException>(action);
+    }
+
+    [Fact]
+    public void MustNotBeNullOrEmpty_ThrowsException_WhenStringIsEmpty()
+    {
+        string stringToTest = string.Empty;
+
+        Action action = () => stringToTest.MustNotBeNullOrEmpty();
+
+        Assert.Throws<ValidationException>(action);
+    }
+}
